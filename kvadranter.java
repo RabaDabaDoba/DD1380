74006e5223689c89c29b49cc32072b3e7dd940c8
RabaDabaDoba <rodah1997@gmail.com>
2021-09-02 23:00:00 +0200

    Update kvadranter.java

--- kvadranter.java
+++ kvadranter.java
@@ -5,8 +5,23 @@ public class kvadranter {
     public static void main(String[] args) {
 
         //System.out.println(raknaKvadrat());
-        int [] pos = {2,5};
-        System.err.println(translateToNumber(pos, 3));
+
+        //int[] pos = {3,3};
+        //System.err.println(translateToNumber(pos, 3));
+        //System.err.println("and now we go back:");
+
+
+        //pos = translateToPos2(pos);
+        //System.err.println(pos[0] + " " + pos[1]);
+
+        raknaKvadrater();
+    }
+
+    private static void raknaKvadrater(){
+        int [] s = {1,4,1}; //From a string
+        int [] pos = translateToPos(s);
+        System.err.println(pos[0] + " " + pos[1]);
+        System.err.println(translateToNumber(new int[]{2,5}, s.length));
     }
 
     private static String raknaKvadrat() {
@@ -42,7 +57,7 @@ public class kvadranter {
             System.err.print(i + ".");
         }
         System.err.println("\n");
-        int[] posXY = translateToPos(pos);
+        int[] posXY = translateToPos2(pos);
         System.err.println("Translates into");
         for (int i : posXY) {
             System.err.print(i + " ");
@@ -66,42 +81,103 @@ public class kvadranter {
 
     }
 
-
     private static String translateToNumber(int[] posXY, int size) {
-
-        //Given a postion (x,y) we should convert it to a number
-        //Ex 4³, so a 8x8 = 64 cells. Given cell at [5][5] we should get a number with 3 digits, being 2.3.2
+        // Given a postion (x,y) we should convert it to a number
+        // Ex 4³, so a 8x8 = 64 cells. Given cell at [5][5] we should get a number with
+        // 3 digits, being 2.3.2
         /*
-        What we do is that we break down each array by only looking on the values. Lets look at it[X][Y]
-        If X is larger then 2^n /2 we go right, otherwise left. If Y is larger then 2^n /2 we go down, else up. 
-        This we do till we have one cell left in a recursive way.
-
-    
-        */
+         * What we do is that we break down each array by only looking on the values.
+         * Lets look at it[X][Y] If X is larger then 2^n /2 we go right, otherwise left.
+         * If Y is larger then 2^n /2 we go down, else up. This we do till we have one
+         * cell left in a recursive way.
+         */
+        String returnNumber = "";
         int row = posXY[0];
         int col = posXY[1];
-        int currentSize = (int)Math.pow(2, size); //Length of the sides
-            if(size == 0) return "";
-            else if(row+1 > currentSize/2 && col+1 <= 2){
-                //This means we're in section 1
-                posXY[0] = row-currentSize/2;
-                posXY[1] = col;
 
-                return "1." + translateToNumber(posXY, size-1);
-            }else if(row+1 > currentSize/2 && col+1 > currentSize/2){
-                //This means we're in section 2
-            }else if(row+1 <= currentSize/2 && col+1 <= currentSize/2){
-                //This means we're in section 3
-            }else if(row+1 <= currentSize/2 && col+1 > currentSize/2){
-                //This means we're in section 4
-                //posXY = {row,col-currentSize/2};
-                posXY[0] = row;
-                posXY[1] = col-currentSize/2;
-                return "4." + translateToNumber(posXY, size-1);
-            
+        int currentSize = (int) Math.pow(2, size); // Length of the sides
+        System.err.println("Translating row: " + row + ", col: " + col + ", with currentSize: " + currentSize);
+        if (row + 1 > currentSize / 2 && col + 1 <= currentSize / 2) {
+            // This means we're in section 1
+            posXY[0] = row - currentSize / 2;
+            posXY[1] = col;
+            returnNumber = "1";
+        } else if (row + 1 > currentSize / 2 && col + 1 > currentSize / 2) {
+            // This means we're in section 2
+            posXY[0] = row - currentSize / 2;
+            posXY[1] = col - currentSize / 2;
+            returnNumber = "2";
+        } else if (row + 1 <= currentSize / 2 && col + 1 <= currentSize / 2) {
+            // This means we're in section 3
+            posXY[0] = row;
+            posXY[1] = col;
+            returnNumber = "3";
+        } else if (row + 1 <= currentSize / 2 && col + 1 > currentSize / 2) {
+            // This means we're in section 4
+            posXY[0] = row;
+            posXY[1] = col - currentSize / 2;
+            returnNumber = "4";
+        } else {
+            System.err.println("NO");
         }
 
-        return "";
+        if (size == 1) {
+            return returnNumber;
+        } else {
+            return returnNumber + "." + translateToNumber(posXY, size - 1);
+        }
+
+    }
+
+    private static int[] translateToPos2(int[] number) {
+
+        
+        int rowMax = (int) Math.pow(2, number.length);
+        int rowMin = 1;
+        int colMax = (int) Math.pow(2, number.length);
+        int colMin = 1;
+        int os = (int)Math.pow(2, number.length);
+        for (int i = 0; i < number.length; i++) {
+            int currentNumber = number[i];
+            int currentSize = (int)Math.pow(2, number.length - i);
+            switch (currentNumber) {
+                case 1:
+                    //Then we know that it is in the bottom left part
+                    //This means: row > currentSize/2, col <= currentSize/2
+                
+
+                    if(rowMin < 1 + currentSize/2)rowMin = 1 + currentSize/2;
+                    if(colMax > currentSize/2)colMax = currentSize/2;
+                    break;
+    
+                case 2:
+                    if(rowMin < 1 + currentSize/2)rowMin = 1 + currentSize/2;
+                    if(colMin < 1 + currentSize/2)colMin = 1 + currentSize/2;
+                    break;
+                case 3:
+                    if(rowMax > currentSize/2) rowMax = currentSize/2;
+                    if(colMax > currentSize/2) colMax = currentSize/2;
+                    break;
+                case 4:
+                    
+                    if(rowMax > currentSize/2)rowMax = currentSize/2;
+                    if(colMin < 1 + currentSize/2) colMin = 1 + currentSize/2;
+                    //colMin = 1 + currentSize/2;
+                    break;
+    
+                default:
+                    System.err.println("Should not come here");
+                    break;
+            }
+
+            System.err.println("After calc for number: " + currentNumber + "minmax values, rowMin: " + rowMin + ", rowMax: " + rowMax + ", colMin: " + colMin
+                    + ", colMax: " + colMax);
+        }
+
+        int [] pos = new int [2];
+        pos[0] = rowMin-1;
+        pos[1] = colMin -1;
+        return pos;
     }
 
     private static int[] translateToPos(int[] pos) {
